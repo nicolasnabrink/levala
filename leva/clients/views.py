7b659b0a5790b10163eb06f3917cf6583be4d546
Luiz Antonio Cardoso dos Santos <luizacs17@usp.br>
2022-11-27 16:35:55 -0300

    clients criada

---
+++ leva/clients/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from user.models import Client
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here
+
+
