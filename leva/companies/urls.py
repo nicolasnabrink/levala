27a88ff89ac9933efadf487954c9fbf9c9cec5f5
Nicolas Helio Cunha Nabrink <nicolas_nabrink@usp.br>
2022-12-05 13:53:58 -0300

    Aplicando Reply

--- leva/companies/urls.py
+++ leva/companies/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import detail_company, list_companies,search_companies
 from . import views
 from user import views as vs
-from user.views import create_pedido, create_comment
 from user.models import User
 
 app_name = 'companies'
@@ -12,6 +11,7 @@ urlpatterns = [
     path('<int:user_id>/', views.detail_company, name='detail'),
     path('update_company_profile/<int:user_id>/',vs.UpdateCompanyProfile.as_view(), name='update_company_profile'),
     path('<int:user_id>/pedido/', vs.create_pedido, name='create_pedido'),
+    path('<int:comment_id>/reply/', vs.create_reply, name='create_reply'),
     path('<int:companyuser_id>/comment/', vs.create_comment, name='create_comment')
 ]
 
